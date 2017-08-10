e84cb75f7e9b231873073aa446fa89b55cc66582
raideer <idmania1@gmail.com>
2017-08-10 18:50:47 +0300

    Basic localization

--- src/PlatronClient.js
+++ src/PlatronClient.js
@@ -1,12 +1,38 @@
 const { AkairoClient } = require('discord-akairo');
+const Localize = require('localize');
+const fs = require('fs');
 
 class PlatronClient extends AkairoClient {
     build() {
+        const translations = JSON.parse(fs.readFileSync(__dirname + '/../translations.json'));
+        if (!translations) {
+            throw "Failed to parse translations.json";
+        }
+
+        const localize = new Localize(translations, null, 'machine');
+        this.localize = localize;
+
         return super.build();
     }
 
     loadAll() {
         super.loadAll();
+    }
+
+    _() {
+        return this.localize.translate.apply(null, arguments);
+    }
+
+    env(key, defaultValue = null) {
+        if (process.env[key]) {
+            return process.env[key];
+        }
+
+        if (typeof defaultValue == 'function') {
+            return defaultValue.call(this);
+        }
+
+        return defaultValue;
     }
 }
 
