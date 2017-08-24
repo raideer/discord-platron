2278d8ec636a5f9053e97df7b18b89eecb765fe9
raideer <idmania1@gmail.com>
2017-08-24 20:55:08 +0300

    Removed old role setting

--- src/listeners/GuildJoinListener.js
+++ src/listeners/GuildJoinListener.js
@@ -14,32 +14,6 @@ function exec(guild) {
             prefix: "!",
             locale: "en"
         }
-    }).spread((item, created) => {
-        const hasRole = guild.roles.find('id', item.verifiedRole);
-
-        if (created) {
-            console.log('Added guild', guild.name, 'to the database');
-        }
-
-        if (!hasRole) {
-            guild.createRole({
-                name: "PlaTRON Verified",
-                color: "BLUE"
-            }).then(role => {
-                item.verifiedRole = role.id;
-                item.save();
-
-                console.log('Created verified role for guild', guild.name, 'as', role.id);
-            }).catch(error => {
-                if (error.code == 50013) {
-                    return console.error('Missing role permissions in guild', guild.name);
-                }
-
-                console.error(error);
-            });
-        } else {
-            console.log('Verified role already exists');
-        }
     });
 }
 
