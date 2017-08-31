c76010eee26a19610a1ce03ba75db2bcdb26e8e7
raideer <idmania1@gmail.com>
2017-08-31 21:42:10 +0300

    Restructured for reusability

--- src/commands/HelpCommand.js
+++ src/commands/HelpCommand.js
@@ -9,6 +9,39 @@ class HelpCommand extends Command {
         });
     }
 
+    getHelp(message, command) {
+        let a = [];
+        let prefix = message.util.prefix;
+        let aliases = (_.rest(command.aliases).length > 0)? `(${_.rest(command.aliases).join(' | ')})` : '';
+        let description = (typeof command.description === 'function')?command.description.call(this):command.description;
+        if (description) {
+            description = '- ' + description;
+        }
+
+        a.push(`:black_small_square: **${_.first(command.aliases)}** ${aliases} ${description}`);
+
+        if (command.usage) {
+            a.push(`${this.client._('bot.command.usage')}: \`${prefix}${command.usage}\``);
+        }
+
+        if (command.usageExamples.length > 0) {
+            a.push(`${this.client._('bot.command.examples')}:`);
+
+            for (let i in command.usageExamples) {
+                a.push(`:white_small_square: \`${prefix}${command.usageExamples[i]}\``);
+            }
+
+            a.push('');
+        }
+
+        if (command.usageNote) {
+            let note = (typeof command.usageNote === 'function')?command.usageNote.call(this):command.usageNote;
+            a.push(note);
+        }
+
+        return a.join('\n');
+    }
+
     exec(message, args) {
         let answers = [`__${this.client._('bot.command.list_of_commands')}__`];
 
@@ -17,36 +50,7 @@ class HelpCommand extends Command {
                 return;
             }
 
-            let a = [];
-
-            let aliases = (_.rest(command.aliases).length > 0)? `(${_.rest(command.aliases).join(' | ')})` : '';
-            let description = (typeof command.description === 'function')?command.description.call(this):command.description;
-            if (description) {
-                description = '- ' + description;
-            }
-
-            a.push(`:black_small_square: **${_.first(command.aliases)}** ${aliases} ${description}`);
-
-            if (command.usage) {
-                a.push(`${this.client._('bot.command.usage')}: \`${command.usage}\``);
-            }
-
-            if (command.usageExamples.length > 0) {
-                a.push(`${this.client._('bot.command.examples')}:`);
-
-                for (let i in command.usageExamples) {
-                    a.push(`:white_small_square: \`${command.usageExamples[i]}\``);
-                }
-
-                a.push('');
-            }
-
-            if (command.usageNote) {
-                let note = (typeof command.usageNote === 'function')?command.usageNote.call(this):command.usageNote;
-                a.push(note);
-            }
-
-            answers.push(a.join('\n'));
+            answers.push(this.getHelp(message, command));
         });
 
         message.channel.send(answers.join('\n'));
