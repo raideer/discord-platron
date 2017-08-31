a95fe071def627778603fff944a806462ac37d0f
raideer <idmania1@gmail.com>
2017-08-31 20:58:34 +0300

    Added 8ball command

---
+++ src/commands/8BallCommand.js
@@ -0,0 +1,40 @@
+const Command = require('../PlatronCommand');
+
+class PingCommand extends Command {
+    constructor() {
+        super('8ball', {
+            aliases: ['8ball']
+        });
+    }
+
+    exec(message) {
+        let options = [
+            this.client._('command.8ball.affirmative1'),
+            this.client._('command.8ball.affirmative2'),
+            this.client._('command.8ball.affirmative3'),
+            this.client._('command.8ball.affirmative4'),
+            this.client._('command.8ball.affirmative5'),
+            this.client._('command.8ball.affirmative6'),
+            this.client._('command.8ball.affirmative7'),
+            this.client._('command.8ball.affirmative8'),
+
+            this.client._('command.8ball.neutral1'),
+            this.client._('command.8ball.neutral2'),
+            this.client._('command.8ball.neutral3'),
+            this.client._('command.8ball.neutral4'),
+            this.client._('command.8ball.neutral5'),
+            this.client._('command.8ball.neutral6'),
+
+            this.client._('command.8ball.negative1'),
+            this.client._('command.8ball.negative2'),
+            this.client._('command.8ball.negative3'),
+            this.client._('command.8ball.negative4'),
+            this.client._('command.8ball.negative5'),
+        ];
+
+        let n = Math.floor(Math.random() * options.length);
+        message.channel.send(`**${options[n]}**`);
+    }
+}
+
+module.exports = PingCommand;
