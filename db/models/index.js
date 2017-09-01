3acf39c3b921d0c4101e460c6df0d74820d3742d
raideer <idmania1@gmail.com>
2017-09-01 15:06:19 +0300

    Fixed db path

--- db/models/index.js
+++ db/models/index.js
@@ -4,13 +4,21 @@ var fs        = require('fs');
 var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(module.filename);
+var env       = process.env.NODE_ENV || 'development';
+var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
-var sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: __dirname + '/../database.sqlite',
-    logging: false
-});
+// var sequelize = new Sequelize({
+//     dialect: 'sqlite',
+//     storage: __dirname + '/../database.sqlite',
+//     logging: false
+// });
+
+if (config.use_env_variable) {
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+} else {
+  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
 fs
   .readdirSync(__dirname)
