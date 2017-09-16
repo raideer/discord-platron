b215bf44a401e11b4422c9ddb8d6fb7dd27e564f
raideer <idmania1@gmail.com>
2017-09-16 15:50:11 +0300

    json to js

---
+++ db/config/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    development: {
+        storage: 'database.sqlite',
+        dialect: 'sqlite',
+        logging: false
+    },
+    production: {
+        storage: 'database.sqlite',
+        dialect: 'sqlite',
+        logging: false
+    }
+};
