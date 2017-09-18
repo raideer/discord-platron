a4958adf5d51dfb71a16e72755fdaa3074beb9f9
raideer <idmania1@gmail.com>
2017-09-18 19:05:06 +0300

    changed find to findOrCreate

--- src/commands/ConfigCommand.js
+++ src/commands/ConfigCommand.js
@@ -1,6 +1,4 @@
 const Command = require('../PlatronCommand');
-const async = require('async');
-const winston = require('winston');
 
 class ConfigCommand extends Command {
     constructor() {
@@ -26,34 +24,33 @@ class ConfigCommand extends Command {
         });
     }
 
-    exec(message, args) {
+    async exec(message, args) {
         const Config = this.client.databases.config.table;
 
         switch (args.action) {
-        case 'set':
-            Config.find({
+        case 'set': {
+            let value = args.value;
+            if (value === 'true') {
+                value = true;
+            } else if (value === 'false') {
+                value = false;
+            }
+            const config = await Config.findOrCreate({
                 where: {
                     field: args.field,
                     guild_id: message.guild.id
-                }
-            }).then(config => {
-                if (config) {
-                    let value = args.value;
-                    if (value === 'true') {
-                        value = true;
-                    } else if (value === 'false') {
-                        value = false;
-                    }
-
-                    config.value = value;
-                    config.save().then(() => {
-                        message.reply(`${args.field} has been set to ${value}`);
-                    });
-                } else {
-                    message.reply(`**${args.field}** doesn't exist`);
+                },
+                defaults: {
+                    value: value
                 }
             });
+
+            config.value = value;
+            config.save().then(() => {
+                message.reply(`${args.field} has been set to ${value}`);
+            });
             break;
+        }
         case 'get':
             Config.find({
                 where: {
