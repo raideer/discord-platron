b2b43f23a0d58f04562450d2ef5c41debb1dbe32
raideer <idmania1@gmail.com>
2017-09-18 15:57:06 +0300

    Updated role model

--- db/models/role.js
+++ db/models/role.js
@@ -1,25 +1,32 @@
 'use strict';
-module.exports = function(sequelize, DataTypes) {
-  var Role = sequelize.define('Role', {
-    id: {
-        type: DataTypes.STRING,
-        primaryKey: true,
-        allowNull: false
-    },
-    type: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    key: {
-        type: DataTypes.STRING,
-        allowNull: false
-    }
-  }, {
-    classMethods: {
-      associate: function(models) {
-          Role.belongsTo(models.Guild);
-      }
-    }
-  });
-  return Role;
+module.exports = (sequelize, DataTypes) => {
+    const Role = sequelize.define('Role', {
+        id: {
+            type: DataTypes.STRING,
+            primaryKey: true,
+            allowNull: false
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: 'guild-name'
+        },
+        group: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        guildId: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: 'guild-name'
+        }
+    }, {
+        classMethods: {
+            associate: models => {
+                Role.belongsTo(models.Guild);
+            }
+        }
+    });
+
+    return Role;
 };
