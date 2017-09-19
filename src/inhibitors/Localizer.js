a25d24458fc3c2031bb50d8172b39f5de2cc863f
raideer <idmania1@gmail.com>
2017-09-19 15:25:20 +0300

    Style fix

--- src/inhibitors/Localizer.js
+++ src/inhibitors/Localizer.js
@@ -1,30 +1,23 @@
 const { Inhibitor } = require('discord-akairo');
 
-function exec(message, command, args) {
-    return new Promise((resolve, reject) => {
-        const content = this.client.commandHandler._parseCommand(message, false).content;
-        command.parse(content, message).then(args => {
-            let locale = 'en'
+const exec = async (message, command) => {
+    const content = this.client.commandHandler._parseCommand(message, false).content;
+    const args = await command.parse(content, message);
+    let locale = 'en';
 
-            if (args._lang) {
-                locale = args._lang;
-            } else if(args._en) {
-                locale = 'en';
-            } else if (args._lv) {
-                locale = 'lv';
-            } else {
-                if (message.guild) {
-                    locale = this.client.databases.guilds.get(message.guild.id, 'locale', 'en');
-                }
-            }
+    if (args._lang) {
+        locale = args._lang;
+    } else if (args._en) {
+        locale = 'en';
+    } else if (args._lv) {
+        locale = 'lv';
+    } else if (message.guild) {
+        locale = this.client.databases.guilds.get(message.guild.id, 'locale', 'en');
+    }
 
-            this.client.localize.setLocale(locale);
-            message.locale = locale;
-
-            resolve();
-        });
-    });
-}
+    this.client.localize.setLocale(locale);
+    message.locale = locale;
+};
 
 module.exports = new Inhibitor('localizer', exec, {
     reason: 'localized'
