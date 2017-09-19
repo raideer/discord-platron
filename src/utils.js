ffe8ab6b1772b6ae2b63dcfce367f6b4c5223247
raideer <idmania1@gmail.com>
2017-09-19 15:15:13 +0300

    Clean up

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,12 @@
 const countries_json = require('./countryCodes.json');
 const { Collection } = require('discord.js');
 const cheerio = require('cheerio');
-const request = require('request');
+const request = require('request-promise');
 
 const countries = (() => {
-    let col = new Collection();
+    const col = new Collection();
 
-    for (let c in countries_json) {
+    for (const c in countries_json) {
         col.set(c, countries_json[c]);
     }
 
@@ -14,7 +14,13 @@ const countries = (() => {
 })();
 
 module.exports = {
+    // List of countries
     countries: countries,
+    /**
+     * Converts an object to a discord.js Collection
+     * @param  {Object} object Object to convert
+     * @returns {Collection}
+     */
     objectToCollection: object => {
         const col = new Collection();
         for (const id in object) {
@@ -22,8 +28,13 @@ module.exports = {
         }
         return col;
     },
-    getFlag: (countryName) => {
-        let country = countries.find((c) => {
+    /**
+     * Returns an emoji flag of the given country
+     * @param  {string} countryName Country
+     * @returns {string}
+     */
+    getFlag: countryName => {
+        const country = countries.find(c => {
             return c.countryName.toLowerCase() == countryName.toLowerCase();
         });
 
@@ -31,79 +42,85 @@ module.exports = {
             return `:flag_${country.iso2.toLowerCase()}:`;
         }
 
-        return "";
+        return '';
     },
-    number: (number) => {
+    /**
+     * Converts a number to a localized number string
+     * eg. 100000 will convert to 100'000
+     * @param  {number/string} number Number to convert
+     * @returns {string} Localized number string
+     */
+    number: number => {
         return Number(number).toLocaleString();
     },
-    strToColor: (str) => {
-        function hashCode(str) {
-            var hash = 0;
+    /**
+     * Converts any string to a color
+     * Two identical strings will return the same color
+     * @param  {string} str Any string
+     * @returns {string/hexcolor}
+     */
+    strToColor: str => {
+        const hashCode = s => {
+            let hash = 0;
             for (var i = 0; i < str.length; i++) {
-               hash = str.charCodeAt(i) + ((hash << 5) - hash);
+                hash = s.charCodeAt(i) + ((hash << 5) - hash);
             }
             return hash;
-        }
+        };
 
-        function intToRGB(i){
+        const intToRGB = i => {
             var c = (i & 0x00FFFFFF)
                 .toString(16)
                 .toUpperCase();
 
-            return "00000".substring(0, 6 - c.length) + c;
+            return '00000'.substring(0, 6 - c.length) + c;
+        };
+
+        return intToRGB(hashCode(str)).toLowerCase().split('')
+        .reduce((result, ch) => (result * 16) + '0123456789abcdefgh'.indexOf(ch), 0);
+    },
+    /**
+     * Scrapes basic citizen information
+     * **Under construction**
+     * @param  {integer}  id  ID of the citizen
+     * @returns {Promise}
+     */
+    getCitizenInfo: async id => {
+        const body = await request.get(`https://www.erepublik.com/en/citizen/profile/${id}`);
+        const $ = cheerio.load(body);
+        const data = {};
+        const $ca = $('.citizen_activity').children();
+
+        const prettify = text => {
+            return String(text).replace(/[\t\n\r]/g, '').replace(/\s+/g, ' ').trim();
+        };
+
+        data.party = prettify($ca.first().find('.noborder span a').text());
+        data.partyRole = prettify($ca.first().find('h3').text());
+
+        return data;
+    },
+    /**
+     * Converts citizen name to ID
+     * @param  {string}  name Citizen name
+     * @returns {Promise} Promise with the citizen id as a parameter
+     */
+    citizenNameToId: async name => {
+        const body = await request.get(`https://www.erepublik.com/en/main/search/?q=${encodeURIComponent(name)}`);
+        const $ = cheerio.load(body);
+
+        const results = $('table.bestof tr');
+
+        if (results.length >= 2) {
+            const profileUrl = $(results[1]).find('.nameholder a').attr('href');
+            if (profileUrl) {
+                const match = profileUrl.match(/profile\/([0-9]+)/);
+                if (match) {
+                    const id = Number(match[1]);
+
+                    return id;
+                }
+            }
         }
-
-        return intToRGB(hashCode(str)).toLowerCase().split('').reduce( (result, ch) =>
-        result * 16 + '0123456789abcdefgh'.indexOf(ch), 0);
-    },
-    getCitizenInfo: (id) => {
-        return new Promise((resolve, reject) => {
-            request.get(`https://www.erepublik.com/en/citizen/profile/${id}`, (error, response, body) => {
-                if (error) {
-                    return reject(error);
-                }
-
-                const $ = cheerio.load(body);
-                let data = {};
-
-                const $ca = $('.citizen_activity').children();
-
-                function prettify(text) {
-                    return String(text).replace(/[\t\n\r]/g, '').replace(/\s+/g, ' ').trim();
-                }
-
-                data.party = prettify($ca.first().find('.noborder span a').text());
-                data.partyRole = prettify($ca.first().find('h3').text());
-
-                return resolve(data);
-            });
-        });
-    },
-    citizenNameToId: (name) => {
-        return new Promise((resolve, reject) => {
-            request.get(`https://www.erepublik.com/en/main/search/?q=${encodeURIComponent(name)}`, (error, response, body) => {
-                if (error) {
-                    return reject(error);
-                }
-
-                const $ = cheerio.load(body);
-
-                const results = $('table.bestof tr');
-
-                if (results.length >= 2) {
-                    const profileUrl = $(results[1]).find('.nameholder a').attr('href');
-                    if (profileUrl) {
-                        const match = profileUrl.match(/profile\/([0-9]+)/);
-                        if (match) {
-                            const id = Number(match[1]);
-
-                            return resolve(id);
-                        }
-                    }
-                }
-
-                return reject();
-            });
-        });
     }
 };
