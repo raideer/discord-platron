273c24b593c6f1e5f30da42173e5fd12c49c8b9c
raideer <idmania1@gmail.com>
2017-09-20 01:03:19 +0300

    Cleanup migration

---
+++ db/migrations/20170919205147-cleanup-guilds.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+    up: async queryInterface => {
+        await queryInterface.removeColumn('Guilds', 'logging');
+        await queryInterface.removeColumn('Guilds', 'roleVerified');
+        await queryInterface.removeColumn('Guilds', 'roleVerifiedEnabled');
+        await queryInterface.removeColumn('Guilds', 'forceErepNames');
+    },
+
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.removeColumn('Guilds', 'roleVerified', {
+            type: Sequelize.STRING
+        });
+        await queryInterface.removeColumn('Guilds', 'roleVerifiedEnabled', {
+            type: Sequelize.BOOLEAN,
+            defaultValue: true
+        });
+        await queryInterface.removeColumn('Guilds', 'forceErepNames', {
+            type: Sequelize.BOOLEAN,
+            defaultValue: false
+        });
+    }
+};
