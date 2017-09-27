f2c354c0ee2b80d7923f8b9796f8de699edb96c4
raideer <idmania1@gmail.com>
2017-09-27 23:18:41 +0300

    Shower thoughts

---
+++ src/commands/ShowerCommand.js
@@ -0,0 +1,27 @@
+const Command = require('../PlatronCommand');
+const request = require('request-promise');
+
+class ShowerCommand extends Command {
+    constructor() {
+        super('showerThougts', {
+            aliases: ['thinking', 'shower'],
+            description: () => {
+                return 'Returns a random shower thought';
+            },
+            usage: 'thinking'
+        });
+    }
+
+    async exec(message) {
+        const thoughts = await request({
+            method: 'GET',
+            json: true,
+            uri: 'https://www.reddit.com/r/showerthoughts/top.json?limit=100&t=week'
+        });
+
+        const i = Math.floor(Math.random() * thoughts.data.children.length);
+        await message.channel.send(`${thoughts.data.children[i].data.title} :thinking:`);
+    }
+}
+
+module.exports = ShowerCommand;
