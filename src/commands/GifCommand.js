498c3e38d113348a19b8d3c2852edc532b804779
raideer <idmania1@gmail.com>
2017-09-27 23:26:22 +0300

    Random gif command

---
+++ src/commands/GifCommand.js
@@ -0,0 +1,27 @@
+const Command = require('../PlatronCommand');
+const request = require('request-promise');
+
+class GifCommand extends Command {
+    constructor() {
+        super('randomGif', {
+            aliases: ['gif'],
+            description: () => {
+                return 'Returns a random shower thought';
+            },
+            usage: 'thinking'
+        });
+    }
+
+    async exec(message) {
+        const thoughts = await request({
+            method: 'GET',
+            json: true,
+            uri: 'https://www.reddit.com/r/gifs/top.json?limit=50&t=week'
+        });
+
+        const i = Math.floor(Math.random() * thoughts.data.children.length);
+        await message.channel.send(thoughts.data.children[i].data.url);
+    }
+}
+
+module.exports = GifCommand;
