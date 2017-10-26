36a7f4e8ff41dc269f06c0c6b5e6d6dfc7f628f5
raideer <idmania1@gmail.com>
2017-10-26 17:34:11 +0300

    Fixed localizer

--- src/inhibitors/Localizer.js
+++ src/inhibitors/Localizer.js
@@ -2,17 +2,27 @@ const { Inhibitor } = require('discord-akairo');
 
 async function exec(message, command) {
     const content = this.client.commandHandler._parseCommand(message, false).content;
-    const args = await command.parse(content, message);
-    let locale = 'en';
 
-    if (args._lang) {
-        locale = args._lang;
-    } else if (args._en) {
-        locale = 'en';
-    } else if (args._lv) {
-        locale = 'lv';
-    } else if (message.guild) {
-        locale = this.client.databases.guilds.get(message.guild.id, 'locale', 'en');
+    let locale = null;
+
+    if (content) {
+        const args = await command.parse(content, message);
+
+        if (args._lang) {
+            locale = args._lang;
+        } else if (args._en) {
+            locale = 'en';
+        } else if (args._lv) {
+            locale = 'lv';
+        }
+    }
+
+    if (!locale) {
+        if (message.guild) {
+            locale = this.client.databases.guilds.get(message.guild.id, 'locale', 'en');
+        } else {
+            locale = 'en';
+        }
     }
 
     this.client.localize.setLocale(locale);
