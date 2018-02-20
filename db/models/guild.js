7b7648baed1477afa29aab6daeca8d3de8c50289
raideer <idmania1@gmail.com>
2018-02-20 20:20:59 +0200

    Removed extra tab

--- db/models/guild.js
+++ db/models/guild.js
@@ -17,7 +17,7 @@ module.exports = (sequelize, DataTypes) => {
     }, {
         classMethods: {
             // associate: models => {
-                // associations can be defined here
+            // associations can be defined here
             // }
         }
     });
